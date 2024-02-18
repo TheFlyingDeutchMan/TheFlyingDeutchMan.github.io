32383d945d35343df487d3d14590156b617578f9
TheFlyingDeutchMan <tim.gerschner.nz@gmail.com>
2024-02-19 11:14:57 +1300

    Me tinkering and figuring out layouts in CSS and vue

---
+++ README.md
@@ -0,0 +1,2 @@
+# TheFlyingDeutchMan.github.io
+The Culmination of my current knowledge to work as a mini portfolio.
---
+++ basic-vue-project/src/views/ProjView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import TheoryItem from '../components/Theory.vue'
+</script>
+<!-- <template>
+  <div class="header">
+  <h1>Projects</h1>
+   -->
+  <!-- </div> -->
+<!-- Testing and probing on how to set up a proper page -->
+<template>
+  <TheoryItem>
+    <template #heading>Test</template>
+    This page is me currently testing some things...
+    <br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Donec sagittis aliquet lacus, eget varius felis scelerisque nec. Integer ac felis nec sapien gravida blandit id eu ligula. Fusce scelerisque massa vitae metus tempor, ac ullamcorper sapien tincidunt. Duis interdum enim vitae tortor posuere, id vestibulum elit tristique. Sed non fermentum sapien, et interdum elit. Vestibulum nec nisl nec lorem iaculis volutpat. Nam nec felis vel justo consequat facilisis. Quisque non pulvinar nisi. Fusce ullamcorper orci nec sapien tempor, id molestie ante gravida. Proin dignissim lectus a commodo volutpat.
+
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus bibendum, magna sed placerat lobortis, lorem ante finibus risus, id congue neque velit eget tortor. Aenean efficitur dui et arcu tempus, sit amet tristique justo pharetra. Nam fermentum efficitur nisi, a vestibulum est commodo sed. Vivamus semper malesuada eros, a ullamcorper purus fermentum in. Ut at odio vel nulla maximus egestas. Vivamus auctor urna ut mollis fermentum. Ut fringilla imperdiet dolor id egestas. Mauris a augue sit amet nunc blandit feugiat. Integer vitae neque sed risus cursus convallis id nec arcu. Maecenas eget fringilla velit, non rhoncus lectus. Sed sollicitudin auctor enim, non dignissim nulla tristique eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam sit amet tortor sit amet mi tempus posuere. Integer nec leo ipsum.
+
+Curabitur id justo neque. Suspendisse potenti. Vestibulum vel purus dui. Sed in massa vel risus pretium rutrum. Duis volutpat pharetra nibh, eu vestibulum sapien tristique id. Integer dapibus, quam sit amet consequat dictum, nulla justo ullamcorper urna, id varius magna erat vel dolor. Vestibulum condimentum erat nisl, vitae suscipit enim dictum nec. Nam suscipit, justo at hendrerit commodo, odio quam dictum justo, sed aliquet lectus eros a odio. Sed nec libero nunc. Aliquam in varius arcu, at dapibus velit. Maecenas in bibendum velit. Nulla facilisi. Suspendisse ac lorem dui. In et quam libero. Duis semper, odio ac rutrum vehicula, ex est sodales leo, id sollicitudin nibh mauris vel dui. Integer et elementum justo.
+  </TheoryItem>
+</template>
+<!-- </template> -->
+<!-- <style> -->
+<!-- /* @media (min-width: 1024px) {
+  .header {
+    min-height: 80lvh;
+    display: flex;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+  h1 {
+    padding-bottom: 0.01px;
+  }  
+} */
+/* .content {
+    /* min-height: 80lvh; */
+    display: flex;
+    text-align: center; 
+    position: relative;
+    margin-top: 2rem;
+    display: flex;
+  } -->
+<!-- </style> -->
